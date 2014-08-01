c835594cd30ffe71a16e70603e98014043ea06d5
fidepus <mail@fidepus.de>
2014-08-01 13:20:45 +0200

    More spaces api stuff

---
+++ maschinendeck/js/spaceapi.js
@@ -0,0 +1,48 @@
+$(document).ready(function($){
+
+    var Application = {
+
+        run: function() {
+            this.view = $("#button, #circle3371, #path3339");
+            this.title = $("h2");
+            this.load_status();
+        },
+
+        load_status: function() {
+
+            $.getJSON("status/json", function(data){
+
+                if(data !== null && typeof data === 'object'
+                    && data.hasOwnProperty('state')
+                    && data.state.hasOwnProperty('open'))
+                {
+                    switch(data.state.open)
+                    {
+                        case true:
+
+                            Application.render('open');
+                            break;
+
+                        case false:
+
+                            Application.render('closed');
+                            break;
+
+                        default:
+
+                            Application.render('undefined');
+                    }
+                }
+            });
+            setTimeout(Application.load_status, 30000);
+        },
+
+        render: function(status) {
+
+            this.title.text(status.toUpperCase());
+            this.view.attr('class', status);
+        }
+    }
+
+    Application.run();
+});
