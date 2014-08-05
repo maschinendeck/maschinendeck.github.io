f16c732179521e3c6feb82a22296ae6e9e162363
Christian Schwarz <me@cschwarz.com>
2014-08-05 23:54:23 +0200

    Implemented call to state.maschinendeck.org API to display current opening state of the hackerspace.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+
+//Call this function to update the space
+function updateSpaceState(spaceStateElement) {
+
+	$.ajax({
+	      dataType: "jsonp",
+	      jsonp: "callback",
+	      url: "http://state.maschinendeck.org/spaceapi.php",
+	      success: function(response){
+	        var statusText = "Status: " + (response.state.open ? "Offen" : "Geschlossen");
+	        var statusField = $("#space_open_status");
+	        spaceStateElement.text(statusText);
+	        if (response.state.open) {
+	          spaceStateElement.css({color: "green"});
+	        } else {
+	        	spaceStateElement.css({});
+	        }
+
+	      }
+	 });
+
+}
