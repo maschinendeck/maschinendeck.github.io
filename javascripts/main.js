0aa5b1d90c27810d784626e4f94ac3a0bd9a78da
Christian Schwarz <me@cschwarz.com>
2014-08-06 23:24:44 +0200

    Switched from JSONP to normal XHR as endpoint now sets `Access-Control-Allow-Origin *`

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,21 +2,17 @@
 //Call this function to update the space
 function updateSpaceState(spaceStateElement) {
 
-	$.ajax({
-	      dataType: "jsonp",
-	      jsonp: "callback",
-	      url: "http://state.maschinendeck.org/spaceapi.php",
-	      success: function(response){
-	        var statusText = "Status: " + (response.state.open ? "Offen" : "Geschlossen");
-	        var statusField = $("#space_open_status");
-	        spaceStateElement.text(statusText);
-	        if (response.state.open) {
-	          spaceStateElement.css({color: "green"});
-	        } else {
-	        	spaceStateElement.css({});
-	        }
+	$.getJSON("http://state.maschinendeck.org/spaceapi.php", function(response){
 
-	      }
-	 });
+		var statusText = "Status: " + (response.state.open ? "Offen" : "Geschlossen");
+        var statusField = $("#space_open_status");
+        spaceStateElement.text(statusText);
+        if (response.state.open) {
+          spaceStateElement.css({color: "green"});
+        } else {
+        	spaceStateElement.css({});
+        }
+
+	});
 
 }
