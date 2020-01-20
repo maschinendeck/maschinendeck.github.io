16710b93c296001f7e4c65e351a04a665e242002
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2020-01-20 14:10:28 +0100

    Re-activated room status

--- js/main.js
+++ js/main.js
@@ -2,16 +2,18 @@
 //Call this function to update the space
 function updateSpaceState(spaceStateElement) {
 
-	$.getJSON("http://state.maschinendeck.org/spaceapi.php", function(response){
+	$.getJSON("http://maschinenstate.42dots.de/", function(response){
+
+		console.log(response);
 
 		var statusText = "Raumstatus: " + (response.state.open ? "Offen" : "Geschlossen");
-        var statusField = $("#space_open_status");
-        spaceStateElement.text(statusText);
-        if (response.state.open) {
-          spaceStateElement.css({color: "green"});
-        } else {
-        	spaceStateElement.css({});
-        }
+    var statusField = $("#space_open_status");
+    spaceStateElement.text(statusText);
+    if (response.state.open) {
+      spaceStateElement.css({color: "green"});
+    } else {
+    	spaceStateElement.css({});
+    }
 
 	});
 
