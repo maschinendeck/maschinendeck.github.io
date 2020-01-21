0d826d28d36bb8a639b0a3630b915f55fce42c78
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2020-01-21 13:17:19 +0100

    Use https for state api call

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 //Call this function to update the space
 function updateSpaceState(spaceStateElement) {
 
-	$.getJSON("http://maschinenstate.42dots.de/", function(response){
+	$.getJSON("https://maschinenstate.42dots.de/", function(response){
 
 		console.log(response);
 
